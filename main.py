6d3d2d66f9562d2402674772ee3450e2383e6dee
Noswad123 <jamal.a.dawson@gmail.com>
2025-03-18 14:00:12 -0400

    fix: extra assignments

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-book_path = "books/frankenstein.txt"
+import sys
+
+# book_path = "books/frankenstein.txt"
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]  # Fix: Correct usage of sys.argv
 with open(book_path) as f:
     contents = f.read()
 word_count = len(contents.split())
@@ -15,6 +22,6 @@ print(f"{word_count} words found in the document ")
 print("\n")
 
 for key,val in letter_dict.items():
-    print(f"The '{key}' character was found {val} times")
+    print(f"{key}: {val}")
 
 print(f"-- End report--")
